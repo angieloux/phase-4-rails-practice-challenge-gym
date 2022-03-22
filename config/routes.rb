69e780615325e665b1ba66919dbf53c9e53b3b70
ANGIELOUX <angelajohnson943@gmail.com>
2022-03-22 19:09:58 -0400

    Scaffold MVC, create model associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "gyms#index"
+  resources :memberships
+  resources :clients
+  resources :gyms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ db/migrate/20220322230421_create_gyms.rb
@@ -0,0 +1,10 @@
+class CreateGyms < ActiveRecord::Migration[7.0]
+  def change
+    create_table :gyms do |t|
+      t.string :name
+      t.text :address
+
+      t.timestamps
+    end
+  end
+end
