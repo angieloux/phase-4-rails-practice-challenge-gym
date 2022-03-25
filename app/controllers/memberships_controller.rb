663bba13bcdd1cf4ec46cd7ffc52406f5a67e11e
ANGIELOUX <angelajohnson943@gmail.com>
2022-03-24 23:30:57 -0400

    Remove ability to sign up to same gym twice, add age validation

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,6 +1,7 @@
 class MembershipsController < ApplicationController
   before_action :set_membership, only: %i[ show edit update destroy ]
   before_action :new_gym_client, only: [ :create ]
+  before_action :set_form_variables, only: [ :new ]
 
   # GET /memberships or /memberships.json
   def index
@@ -13,9 +14,7 @@ class MembershipsController < ApplicationController
 
   # GET /memberships/new
   def new
-    @membership = Membership.new
-    @clients = Client.all
-    @gyms = Gym.all
+
   end
 
   # GET /memberships/1/edit
@@ -26,11 +25,16 @@ class MembershipsController < ApplicationController
   def create
     @membership = Membership.new(membership_params)
     respond_to do |format|
-        if @membership.save && new_gym_client
+        if new_gym_client && @membership.save
           format.html { redirect_to membership_url(@membership), notice: "Membership was successfully created." }
           format.json { render :show, status: :created, location: @membership }
+        elsif !new_gym_client
+          set_form_variables
+          format.html { redirect_to new_membership_url, notice: "Cannot sign up to the same gym twice!" }
+          format.json { render json: @membership.errors, status: :unprocessable_entity }
         else
-          format.html { render :new, status: :unprocessable_entity }
+          set_form_variables
+          format.html { redirect_to new_membership_url, status: :unprocessable_entity }
           format.json { render json: @membership.errors, status: :unprocessable_entity }
         end 
 
@@ -61,17 +65,17 @@ class MembershipsController < ApplicationController
   end
 
   def new_gym_client
-    @client = Client.find(params[:membership][:client_id])
-    @gym =  Gym.find(params[:membership][:gym_id])
-    @client.memberships.ids.include?(@gym.id) ? false : true
-    p "************************************"
-    @clients = Client.client_memberships
-
+    Membership.where(gym_id: params[:membership][:gym_id]).where(client_id: params[:membership][:client_id]).count == 0 ? true : false 
   end
+
   private
     # Use callbacks to share common setup or constraints between actions.
-   
-    
+         
+    def set_form_variables
+      @clients = Client.all
+      @gyms = Gym.all
+    end 
+
     def set_membership
       @membership = Membership.find(params[:id])
     end
--- app/models/client.rb
+++ app/models/client.rb
@@ -4,7 +4,7 @@ class Client < ApplicationRecord
     accepts_nested_attributes_for :memberships
 
     validates :name, presence: true
-    validates :age, presence: true, numericality: {only_integer: true}
+    validates :age, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 18, message: '-> Must be older than 18 to sign up'}
 
     scope :client_memberships, -> {Client.joins(:memberships)}
 
